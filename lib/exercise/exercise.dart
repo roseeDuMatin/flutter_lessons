cc05a0a402f762ecaedc3f984ed4ef7a300d419e
Ecalle Thomas <thomasecalle@hotmail.fr>
2020-04-05 11:04:16 +0200

    Add first lesson

---
+++ lib/exercise/exercise.dart
@@ -0,0 +1,8 @@
+/// Learn how to run the App widget
+/// In this lesson we are going to speak about:
+/// * runApp method
+/// * StatelessWidget
+/// * Container
+/// * MaterialApp
+/// * InheritedWidget
+/// * Theme
---
+++ lib/solution/solution.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        textTheme: TextTheme(
+          title: TextStyle(
+            color: Colors.amber,
+            fontSize: 22,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+      home: Home(),
+    );
+  }
+}
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.white,
+      child: Center(
+        child: Text(
+          "Hello",
+          style: Theme.of(context).textTheme.title,
+        ),
+      ),
+    );
+  }
+}
