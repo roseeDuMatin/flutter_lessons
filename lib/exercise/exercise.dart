ef811f8099eb9a748c5195c7f8384f6a9920f1c8
Ecalle Thomas <thomasecalle@hotmail.fr>
2020-04-05 21:45:11 +0200

    Add 2nd lesson

--- lib/exercise/exercise.dart
+++ lib/exercise/exercise.dart
@@ -1,8 +1,43 @@
-/// Learn how to run the App widget
+import 'package:flutter/material.dart';
+
+/// Learn how to run use a Scaffold widget
 /// In this lesson we are going to speak about:
-/// * runApp method
-/// * StatelessWidget
-/// * Container
-/// * MaterialApp
-/// * InheritedWidget
-/// * Theme
+/// * Scaffold
+/// * BottomNavigationBar
+/// * FloatingActionButton
+/// * AppBAr
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        textTheme: TextTheme(
+          title: TextStyle(
+            color: Colors.amber,
+            fontSize: 22,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+      home: Home(),
+    );
+  }
+}
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.white,
+      child: Center(
+        child: Text(
+          "Hello",
+          style: Theme.of(context).textTheme.title,
+        ),
+      ),
+    );
+  }
+}
