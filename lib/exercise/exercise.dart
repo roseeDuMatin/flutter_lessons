2bbb11d75141be84b320b379138b9db4ede41002
Ecalle Thomas <thomasecalle@hotmail.fr>
2020-04-07 19:47:02 +0200

    Add lesson 6 exercise template

---
+++ lib/exercise/exercise.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'package:flutterlessons/exercise/user.dart';
+import 'package:flutterlessons/exercise/user_item.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        textTheme: TextTheme(
+          title: TextStyle(
+            color: Colors.amber,
+            fontSize: 22,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+      home: Home(),
+    );
+  }
+}
+
+class Home extends StatelessWidget {
+  final List<User> _users = List.generate(100, (int index) {
+    return User(
+      "Last Name $index",
+      "First Name $index",
+      "$index, rue du faubourg saint antoine",
+    );
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: ListView.builder(
+          itemCount: _users.length,
+          itemBuilder: (BuildContext context, int index) {
+            return UserItem(user: _users[index],);
+          },
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/exercise/user.dart
@@ -0,0 +1,7 @@
+class User {
+  final String lastName;
+  final String firstName;
+  final String address;
+
+  User(this.lastName, this.firstName, this.address);
+}
