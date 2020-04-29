76861e0c763fa7fde3549a5eb2d6b9fafc7752b8
roseeDuMatin <s_pelluet@hotmail.fr>
2020-04-29 11:56:47 +0200

    chapitre fini

--- lib/exercise/exercise.dart
+++ lib/exercise/exercise.dart
@@ -7,6 +7,15 @@ import 'package:flutter/material.dart';
 /// * FloatingActionButton
 /// * AppBAr
 
+///
+///
+
+int myFunction() {
+  return 42;
+}
+
+int myFunction2() => 24;
+
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
@@ -16,7 +25,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         textTheme: TextTheme(
           title: TextStyle(
-            color: Colors.amber,
+            color: Colors.black,
             fontSize: 22,
             fontWeight: FontWeight.bold,
           ),
@@ -27,14 +36,51 @@ class MyApp extends StatelessWidget {
   }
 }
 
+void _toto() {
+  print("ok");
+}
+
 class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.white,
-      child: Center(
+    return Scaffold(
+      backgroundColor: Colors.amber,
+      appBar: AppBar(
+        title: Text(
+          "Ma super application",
+          style: Theme.of(context).textTheme.subtitle,
+        ),
+
+        // leading : widget à gauche de la barre
+        leading: Icon(
+          Icons.local_airport,
+          color: Colors.red,
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _toto,
+        child: Icon(Icons.add),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.home,
+            ),
+            title: Text("Home", style: Theme.of(context).textTheme.subtitle),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.favorite,
+            ),
+            title:
+                Text("Favorite", style: Theme.of(context).textTheme.subtitle),
+          ),
+        ],
+      ),
+      body: Center(
         child: Text(
-          "Hello",
+          "Salut",
           style: Theme.of(context).textTheme.title,
         ),
       ),
